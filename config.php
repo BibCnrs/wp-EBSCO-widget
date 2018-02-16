7735cf0e37a5c98f77fb0aaf86a4a96aaa8703f2
fjoin <florence.join@gmail.com>
2018-02-16 16:04:02 +0100

    version 2.1.3

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = (object)[
     'tag' => 'ebsco_widget',
     'name' => 'EBBSCO Widget',
-    'version' => '2.1.2',
+    'version' => '2.1.3',
     'settings' => [
         'url' => [
             'description' => 'Url pour accéder à BibCnrs Api.',
