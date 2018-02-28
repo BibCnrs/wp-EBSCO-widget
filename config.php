e4c295a06e5600074a37287c86a495d8cf24db9e
ThieryMichel <thiery@marmelab.com>
2018-02-28 10:32:13 +0100

    update ebsco-widget to 2.2.0-prod

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = (object)[
     'tag' => 'ebsco_widget',
     'name' => 'EBBSCO Widget',
-    'version' => '3.3.0',
+    'version' => '3.4.0',
     'settings' => [
         'url' => [
             'description' => 'Url pour accéder à BibCnrs Api.',
