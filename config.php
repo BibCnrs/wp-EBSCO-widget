95a22affe30bfb56e7285256ff5d88d4cfd90018
Robin Bressan <robin@buddey.net>
2020-02-06 12:14:00 +0100

    version 3.9.4

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = (object)[
     'tag' => 'ebsco_widget',
     'name' => 'EBBSCO Widget',
-    'version' => '3.9.3',
+    'version' => '3.9.4',
     'settings' => [
         'url' => [
             'description' => 'Url pour accéder à BibCnrs Api.',
