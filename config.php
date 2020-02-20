887b27a3b376be8624b82911cbb7e65f4b8849b3
nacimgoura <nacim.goura.dev@gmail.com>
2020-02-20 16:35:52 +0100

    version 3.10.3

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = (object)[
     'tag' => 'ebsco_widget',
     'name' => 'EBBSCO Widget',
-    'version' => '3.10.2',
+    'version' => '3.10.3',
     'settings' => [
         'url' => [
             'description' => 'Url pour accéder à BibCnrs Api.',
