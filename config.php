ca1e1caf03abebf8d384eecb1506f1cd2e3e2ed1
join <florence.join@inist.fr>
2020-07-31 07:29:22 +0200

    version 3.15.14

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = (object)[
     'tag' => 'ebsco_widget',
     'name' => 'EBBSCO Widget',
-    'version' => '3.15.13',
+    'version' => '3.15.14',
     'settings' => [
         'url' => [
             'description' => 'Url pour accéder à BibCnrs Api.',
