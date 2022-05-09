d700d8e53840f7279c46ecc82dfd091dafcf6921
join <florence.join@inist.fr>
2022-05-09 17:21:08 +0200

    3.16.1

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = (object)[
     'tag' => 'ebsco_widget',
     'name' => 'EBBSCO Widget',
-    'version' => '3.16.0',
+    'version' => '3.16.1',
     'settings' => [
         'url' => [
             'description' => 'Url pour accéder à BibCnrs Api.',
