a8954979a4882c9bb3b3e600887400f567a2d84c
thibault-barrat <contact@thibault-barrat.com>
2022-11-09 11:29:35 +0100

    version 3.17.2

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = (object)[
     'tag' => 'ebsco_widget',
     'name' => 'EBBSCO Widget',
-    'version' => '3.17.1',
+    'version' => '3.17.2',
     'settings' => [
         'url' => [
             'description' => 'Url pour accéder à BibCnrs Api.',
